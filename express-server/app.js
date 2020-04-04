0c31fd1bef2f0876e533c9ffe1ab5af07d66e17d
Alejandra Álvarez <ale.alvarez97@hotmail.com>
2020-04-04 16:39:28 -0600

    update

--- express-server/app.js
+++ express-server/app.js
@@ -1,9 +1,10 @@
-var express = require("express");
+var express = require("express"), cors = require('cors');
 var app = express();
 app.use(express.json());
 app.listen(3000, () => console.log("Server running on port 3000"));
 
-app.get("/url", (req, res, next) => res.json(["Paris", "Barcelona", "Montevideo", "Santiago de Chile", "Mexico DF", "New York"]));
+var ciudades = ["Paris", "Barcelona", "Montevideo", "Santiago de Chile", "Mexico DF", "Nueva york"]
+app.get("/ciudades", (req, res, next) => res.json(ciudades.filter((c) => c.toLowerCase().indexOf(req.query.q.toString().toLowerCase()) > -1 )));
 
 var misDestinos = [];
 app.get("/my", (req, res, next) => res.json(misDestinos));
