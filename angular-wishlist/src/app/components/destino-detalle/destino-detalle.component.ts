2b1a300bca91cab1e59f2bd532caec955ecde371
Alejandra Álvarez <ale.alvarez97@hotmail.com>
2020-04-11 00:22:25 -0600

    rest

--- angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.ts
+++ angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.ts
@@ -1,53 +1,18 @@
-import {Component, Inject, InjectionToken, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {DestinosApiClientModel} from '../../models/destinos-api-client.model';
 import {DestinoViaje} from '../../models/destino-viaje.model';
-import {AppState} from '../../app.module';
-import {Store} from '@ngrx/store';
-
-class DestinosApiClientViejo {
-  getById(id: string): DestinoViaje {
-    console.log('llamado por la clase vieja!');
-    return null;
-  }
-}
-
-interface AppConfig {
-  apiEndpoint: string;
-}
-
-const APP_CONIFG_VALUE: AppConfig = {
-  apiEndpoint: 'mi_api.com'
-};
-
-const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
-
-class DesinosApliClientDecorated extends DestinosApiClientModel {
-  constructor(@Inject(APP_CONFIG) private config: AppConfig, store: Store<AppState>) {
-    // @ts-ignore
-    super(store);
-  }
-  getById(id: string): DestinoViaje {
-    console.log('llamado por la clase decorada!');
-    console.log('config: ' + this.config.apiEndpoint);
-    return super.getById(id);
-  }
-}
 
 @Component({
   selector: 'app-destino-detalle',
   templateUrl: './destino-detalle.component.html',
   styleUrls: ['./destino-detalle.component.css'],
-  providers: [
-    {provide: APP_CONFIG, useValue: APP_CONIFG_VALUE},
-    {provide: DestinosApiClientModel, useClass: DesinosApliClientDecorated},
-    {provide: DestinosApiClientViejo, useExisting: DestinosApiClientModel}
-  ]
+  providers: [ DestinosApiClientModel ]
 })
 export class DestinoDetalleComponent implements OnInit {
   destino: DestinoViaje;
 
-  constructor(private route: ActivatedRoute, private destinosApiClientModel: DestinosApiClientViejo) {
+  constructor(private route: ActivatedRoute, private destinosApiClientModel: DestinosApiClientModel) {
   }
 
   ngOnInit() {
--- angular-wishlist/src/app/models/destinos-api-client.model.ts
+++ angular-wishlist/src/app/models/destinos-api-client.model.ts
@@ -1,18 +1,49 @@
+import {forwardRef, Inject, Injectable} from '@angular/core';
 import {DestinoViaje} from './destino-viaje.model';
-import {BehaviorSubject, Subject} from 'rxjs';
 import {Store} from '@ngrx/store';
-import {AppState} from '../app.module';
+import {APP_CONFIG, AppConfig, AppState} from '../app.module';
 import {ElegidoFavoritoAction, NuevoDestinoAction} from './destinos-viajes-state.model';
-import {Injectable} from '@angular/core';
+import {HttpClient, HttpClientModule, HttpHeaders, HttpRequest, HttpResponse} from '@angular/common/http';
 
 @Injectable()
 export class DestinosApiClientModel {
+  destinos: DestinoViaje[] = [];
 
-  constructor(private store: Store<AppState>) {
+  constructor(
+    private store: Store<AppState>,
+    @Inject(forwardRef(() => APP_CONFIG)) private config: AppConfig,
+    private http: HttpClient
+    ) {
+    this.store
+      .select(state => state.destinos)
+      .subscribe((data) => {
+        console.log('destinos sub store');
+        console.log(data);
+        this.destinos = data.items;
+      });
+    this.store
+      .subscribe((data) => {
+        console.log('all store');
+        console.log(data);
+      });
   }
 
   add(d: DestinoViaje) {
-    this.store.dispatch(new NuevoDestinoAction(d));
+    const headers: HttpHeaders = new HttpHeaders({'X-API-TOKEN': 'token-seguridad'});
+    const req = new HttpRequest('POST', this.config.apiEndpoint + '/my', {nuevos: d.nombre},{headers});
+    this.http.request(req).subscribe((data: HttpResponse<{}>) => {
+      id (data.status === 200){
+        this.store.dispatch(new NuevoDestinoAction(d));
+      }
+    });
+  }
+
+  getById(id: string): DestinoViaje {
+    return this.destinos.filter(function (d) {return d.id.toString() === id; }) [0];
+  }
+
+  getAll(): DestinoViaje[] {
+    return this.destinos;
   }
 
   elegir(d: DestinoViaje) {
